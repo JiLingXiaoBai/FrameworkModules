a96bf9ae0b195bcd5479f0e6471163a70c580172
JiLingXiaoBai <uiojkl3588@qq.com>
2023-07-25 15:23:37 +0800

    playable 学习

--- Assets/Test/TestScripts/TestPlayable/CustomAnimationController.cs
+++ Assets/Test/TestScripts/TestPlayable/CustomAnimationController.cs
@@ -8,12 +8,7 @@ using UnityEngine.Animations;
 public class CustomAnimationController : MonoBehaviour
 {
     private PlayableGraph m_Graph;
-    private AnimationPlayableOutput m_Output;
-    private AnimationMixerPlayable m_Mixer;
-    public AnimationClip clip0;
-    public AnimationClip clip1;
-    [Range(0, 1)]
-    public float weight = 0;
+    public AnimationClip[] clipsToPlay;
 
 
     private void Start()
@@ -22,23 +17,18 @@ public class CustomAnimationController : MonoBehaviour
         GraphVisualizerClient.Show(m_Graph);
         m_Graph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
 
-        m_Mixer = AnimationMixerPlayable.Create(m_Graph, 2);
-        AnimationClipPlayable clipPlayable0 = AnimationClipPlayable.Create(m_Graph, clip0);
-        AnimationClipPlayable clipPlayable1 = AnimationClipPlayable.Create(m_Graph, clip1);
-        m_Graph.Connect(clipPlayable0, 0, m_Mixer, 0);
-        m_Graph.Connect(clipPlayable1, 0, m_Mixer, 1);
-        m_Mixer.SetInputWeight(0, 1);
-        m_Mixer.SetInputWeight(1, 0);
+        var custPlayable = ScriptPlayable<CustomAnimationControllerPlayable>.Create(m_Graph);
+        var playQueue = custPlayable.GetBehaviour();
+        playQueue.Initialize(clipsToPlay, custPlayable, m_Graph);
+        var playableOutput = AnimationPlayableOutput.Create(m_Graph, "Animation", GetComponent<Animator>());
+        playableOutput.SetSourcePlayable(custPlayable, 0);
 
-        m_Output = AnimationPlayableOutput.Create(m_Graph, "Animation", GetComponent<Animator>());
-        m_Output.SetSourcePlayable(m_Mixer);
         m_Graph.Play();
     }
 
     private void Update()
     {
-        m_Mixer.SetInputWeight(0, 1 - weight);
-        m_Mixer.SetInputWeight(1, weight);
+
     }
 
     private void OnDestroy()
